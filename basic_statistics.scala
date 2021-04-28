c92097bf49b18059fef6ba177ac2ee1416b055f2
IngBermudez <alberto.bermudez16@tectijuana.edu.mx>
2021-04-27 23:03:01 -0700

    codigos explicado, correlacion

--- basic_statistics.scala
+++ basic_statistics.scala
@@ -1,21 +1,29 @@
 //correlation
 
+//Empezamos con esta libreria para tener acceso a matrices locales y Métodos de fábrica para Vector.
 import org.apache.spark.ml.linalg.{Matrix, Vectors}
+//libreria para usar el metodo de correlacion
 import org.apache.spark.ml.stat.Correlation
+//permite acceder a un valor de una fila a través del acceso genérico por ordinal,  así como el acceso primitivo
 import org.apache.spark.sql.Row
 
+//Crea vectores densos y dispersos  a partir de sus valores, dentro de la matriz 
 val data = Seq(
-  Vectors.sparse(4, Seq((0, 1.0), (3, -2.0))),
+   (4, Seq((0, 1.0), (3, -2.0))),
   Vectors.dense(4.0, 5.0, 0.0, 3.0),
   Vectors.dense(6.0, 7.0, 0.0, 8.0),
   Vectors.sparse(4, Seq((0, 9.0), (3, 1.0)))
 )
 
+//Se extraen los datos de nuestra matriz y se crea un dataframe respecto a las caracteristicas 
 val df = data.map(Tuple1.apply).toDF("features")
+//Se crea la matriz de correlacion Pearson usando el dataframe que acabamos de crear y le pedimos los primeros valores con head
 val Row(coeff1: Matrix) = Correlation.corr(df, "features").head
+//imprimimos el resultado
 println(s"Pearson correlation matrix:\n $coeff1")
-
+//Se crea la matriz de correlacion Spearman usando el dataframe que acabamos de crear y le pedimos los primeros valores con head
 val Row(coeff2: Matrix) = Correlation.corr(df, "features", "spearman").head
+//imprimimos el resultado
 println(s"Spearman correlation matrix:\n $coeff2")
 
 
